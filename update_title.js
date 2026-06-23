13e6baa00b6e60cdc7b5dc71d3c70362107cfaee
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2026-06-23 20:42:47 -0300

    Tentando alterar fonte

--- update_title.js
+++ update_title.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const https = require('https');
 
-const fontUrl = 'https://fonts.gstatic.com/s/firacode/v27/uU9eCBsR6Z2vfE9aq3bL0fxyUs4tcw4W_A9sFVc.ttf';
+const fontUrl = 'https://fonts.gstatic.com/s/jetbrainsmono/v24/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8L6tjPQ.ttf';
 
 https.get(fontUrl, (res) => {
   const chunks = [];
@@ -19,14 +19,14 @@ https.get(fontUrl, (res) => {
     const injection = `
 <style id="custom-font">
   @font-face {
-    font-family: 'Fira Code Custom';
+    font-family: 'JetBrains Mono Custom';
     font-style: normal;
-    font-weight: 500;
-    src: url(data:font/truetype;charset=utf-8;base64,${fontBase64}) format('truetype');
+    font-weight: bold;
+    src: url(data:font/truetype;base64,${fontBase64}) format('truetype');
   }
 </style>
 <g transform="translate(1240, 150)">
-  <text style="font-family: 'Fira Code Custom', Consolas, 'Courier New', monospace; filter: drop-shadow(0px 10px 0px rgba(191, 64, 100, 1));" font-size="100" font-weight="bold" fill="#ffffff" text-anchor="end">
+  <text style="font-family: 'JetBrains Mono Custom'; filter: drop-shadow(0px 10px 0px rgba(191, 64, 100, 1));" font-size="100" font-weight="bold" fill="#ffffff" text-anchor="end">
     <tspan x="0" dy="0">Commit's</tspan>
     <tspan x="-30" dy="90">City</tspan>
   </text>
